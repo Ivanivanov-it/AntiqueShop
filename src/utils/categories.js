594e953bbd709db1f56198710d06f4f1217017a0
ChunChun20 <113472390+Ivanivanov-it@users.noreply.github.com>
2026-04-24 08:22:12 +0300

    added subcategories for each cateogry in the category page

--- src/utils/categories.js
+++ src/utils/categories.js
@@ -1,14 +1,77 @@
 export const categories = [
-  { slug: 'antikvarni-chasovnici', name: 'Антикварни часовници', description: 'Джобни, стенни, ръчни и настолни часовници.' },
-  { slug: 'porcelan', name: 'Порцелан', description: 'Стари изделия от порцелан и керамика.' },
-  { slug: 'starinni-bizhuta', name: 'Старинни бижута', description: 'Етнографски накити и страринни бижута.' },
-  { slug: 'izobrazitelno-izkustvo', name: 'Изобразително изкуство', description: 'Оригинални картини и редки литографии.' },
-  { slug: 'kolektsionerski-predmeti', name: 'Колекционерски предмети', description: 'Уникални предмети, които разказват история.' },
-  { slug: 'etnika-i-folklor', name: 'Етника и Фолклор', description: 'Исторически облекла и аксесоари.' },
-  { slug: 'suveniri-i-podaratsi', name: 'Сувенири и Подараци', description: 'Сувенири и подаръци с антикварна стойност.' },
-  { slug: 'instrumenti-i-nozhove', name: 'Инструменти и Ножове', description: 'Антикварни инструменти и ножове с колекционерска стойност.' },
-  { slug: 'stari-uredi-i-aparati', name: 'Стари Уреди и Апарати', description: 'Антикварни уреди и апарати с историческа стойност.' },
-  { slug: 'voenni-predmeti', name: 'Военни предмети', description: 'Антикварни военни предмети и артефакти.' }
+  { 
+    slug: 'antikvarni-chasovnici', 
+    name: 'Антикварни часовници', 
+    description: 'Джобни, стенни, ръчни и настолни часовници.',
+    subcategories: [
+      { slug: 'dzhobni-chasovnici', name: 'Джобни часновници' },
+      { slug: 'stenni-chasovnici', name: 'Стенни часовници' },
+      { slug: 'rachni-chasovnici', name: 'Ръчни часовници' },
+      { slug: 'nastolni-chasovnici', name: 'Настолни часовници' },
+      { slug: 'drugi', name: 'Други' }
+    ]
+  },
+  { 
+    slug: 'porcelan', 
+    name: 'Порцелан', 
+    description: 'Стари изделия от порцелан и керамика.',
+    subcategories: []
+  },
+  { 
+    slug: 'starinni-bizhuta', 
+    name: 'Старинни бижута', 
+    description: 'Етнографски накити и страринни бижута.',
+    subcategories: []
+  },
+  { 
+    slug: 'izobrazitelno-izkustvo', 
+    name: 'Изобразително изкуство', 
+    description: 'Оригинални картини и редки литографии.',
+    subcategories: [
+      { slug: 'jivopis', name: 'Живопис' },
+      { slug: 'grafika', name: 'Графика' },
+      { slug: 'skulptura', name: 'Скулптура' }
+    ]
+  },
+  { 
+    slug: 'kolektsionerski-predmeti', 
+    name: 'Колекционерски предмети', 
+    description: 'Уникални предмети, които разказват история.',
+    subcategories: [
+      { slug: 'moneti', name: 'Монети' },
+      { slug: 'medali-i-ordeni', name: 'Медали и ордени' },
+      { slug: 'filatelia', name: 'Филателия' },
+      { slug: 'znachki', name: 'Значки' },
+      { slug: 'drugi', name: 'Други' }
+    ]
+  },
+  { 
+    slug: 'etnika-i-folklor', 
+    name: 'Етника и Фолклор', 
+    description: 'Исторически облекла и аксесоари.',
+     subcategories: [
+      { slug: 'obleklo', name: 'Облекло' },
+      { slug: 'nakiti', name: 'Накити' },
+      { slug: 'drugi', name: 'Други' }
+    ]
+  },
+  { 
+    slug: 'suveniri-i-podaratsi', 
+    name: 'Сувенири и Подараци', 
+    description: 'Сувенири и подаръци с антикварна стойност.',
+    subcategories: []
+  },
+  { 
+    slug: 'knigi', 
+    name: 'Книги', 
+    description: 'Книги',
+    subcategories: [] },
+  { 
+    slug: 'voenni-predmeti', 
+    name: 'Военни предмети', 
+    description: 'Антикварни военни предмети и артефакти.',
+    subcategories: []
+  }
 ];
 
 export const getSlug = (category) => {
